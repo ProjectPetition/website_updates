25d9c919723630f5e86170c69ef8170870d2a468
William May <williamcmay@live.com>
2016-04-09 13:37:11 -0400

    Added daily signatureCount update in the petition table for Rahul's wordcloud

--- scrape.R
+++ scrape.R
@@ -2,12 +2,7 @@
 
 # create database:
 ## create database website;
-## days = paste(paste0("day", 1:30, " int"), collapse = ", ")
-## lowers = paste(paste0("lower", 1:30, " int"), collapse = ", ")
-## uppers = paste(paste0("upper", 1:30, " int"), collapse = ", ")
-## table1 = "CREATE TABLE petitions (id int(7) NOT NULL PRIMARY KEY, type tinytext, title text, body mediumtext, signatureThreshold int, signatureCount int, url text, deadline int, created int, status tinytext, "
-## table2 = ");"
-## command = paste0(table1, days, ", ", lowers, ", ", uppers, table2)
+## CREATE TABLE petitions (id int(7) NOT NULL PRIMARY KEY, type tinytext, title text, body mediumtext, signatureThreshold int, signatureCount int, url text, deadline int, created int, status tinytext);
 ## CREATE TABLE responses (id int(7) NOT NULL PRIMARY KEY, url text, title text, date date);
 ## CREATE TABLE signatureCounts (id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, petition_id int(7) NOT NULL, date date NOT NULL, count int, cumulative int NOT NULL, forecast boolean NOT NULL, upperBound int, lowerBound int, foreign key(petition_id) references petitions(id));
 
@@ -15,19 +10,24 @@ library(jsonlite)
 library(RMySQL)
 
 url = "https://api.whitehouse.gov/v1/petitions.json?limit=999&status=open"
-reg_file = "/network/rit/lab/projpet/will/reg.RData"
-## reg_file = "/home/will/ppet/website/reg.RData"
+#reg_file = "/network/rit/lab/projpet/will/reg.RData"
+reg_file = "/home/will/ppet/website_updates/reg.RData"
 
 get_sigs = function(id, nsigs) {
   offset = 0
   created = vector()
   while (offset < nsigs) {
-    json = fromJSON(paste0("https://api.whitehouse.gov/v1/petitions/",
-        id, "/signatures.json?offset=", offset))
-    created = c(created, json$results$created)
-    offset = offset + 1000
+    json = NA
+    # 'try' because the server randomly gives bogus 404 errors
+    try(json <- fromJSON(paste0("https://api.whitehouse.gov/v1/petitions/", id, "/signatures.json?offset=", offset)))
+    if (!is.na(json)) {
+      # if the API call was successful, get the data and move on, else
+      # we're trying again
+      created = c(created, json$results$created)
+      offset = offset + 1000
+    }
     cat(paste0(offset, "\n"))
-    Sys.sleep(.2)
+    Sys.sleep(1)
   }
   created
 }
@@ -45,8 +45,20 @@ update_sigs = function(p, con, counts) {
 
   sigs = get_sigs(p$id, p$signatureCount)
   sig_counts =
-    table(as.Date(as.POSIXct(sigs, origin = "1970-01-01")))
+    table(as.Date(as.POSIXct(sigs, origin = "1970-01-01"),
+                  tz = "America/New_York"))
+
   ndays = min(30, Sys.Date() - p$creation_date)
+  
+  # quick test to see if WtP is acting strange. Will prevent
+  # short-lived problems, but if things are screwy multiple days in a
+  # row it'll still become an issue
+  yesterday = sig_counts[as.character(Sys.Date() - 1)]
+  daybefore = sig_counts[as.character(Sys.Date() - 2)]
+  if (!is.na(daybefore)) {
+    is_screwy = is.na(yesterday) && daybefore > 100
+    if (is_screwy) return(FALSE)
+  }
   
   dates = p$creation_date + 0:(ndays-1)
   days = setNames(sig_counts[as.character(dates)], dates)
@@ -72,11 +84,10 @@ update_forecast = function(p, con, days, reg, forecast_exists) {
   cur_count = sum(days)
   ndays = length(days)
   newdata =
-    data.frame(l1 = log(days[ndays] + 1),
-               l2 = log(days[ndays - 1] + 1))
+    data.frame(l1 = log(days[ndays] + 1))
   # the dates being forecasted (starts with today)
-  dates = Sys.Date() + 0:30
-  for (n in 1:(30 - ndays)) {
+  dates = Sys.Date() + 0:31
+  for (n in 1:(31 - ndays)) {
     reg1 = reg[[ndays]][[n]]
     ## prediction = predict(reg1, newdata = newdata,
     ##     interval = "confidence")
@@ -121,11 +132,19 @@ for (row in 1:nrow(petitions)) {
 
   # get the age of the petition, in days
   p$creation_date =
-    as.Date(as.POSIXct(p$created, origin = "1970-01-01"))
+    as.Date(as.POSIXct(p$created, origin = "1970-01-01"),
+            tz = "America/New_York")
   ndays = Sys.Date() - p$creation_date
 
   # add to petitions table, if needed
-  if (!(p$id %in% pets$id)) add_to_table(p, con, vals)
+  if (!(p$id %in% pets$id)) {
+    add_to_table(p, con, vals)
+  } else {
+    # update the signatureCount, at least (for Rahul's wordcloud)
+    q = paste0("update petitions set signatureCount=",
+        p$signatureCount, " where id=", p$id)
+    dbSendQuery(con, q)
+  }
 
   # check to see if database contains signature counts already
   counts_exist = any(counts$petition_id == p$id)
@@ -136,7 +155,7 @@ for (row in 1:nrow(petitions)) {
     days = update_sigs(p, con, counts_exist)
 
     # update forecast too, if appropriate
-    if (ndays > 1 && ndays < 30 && sum(days) >= 150) {
+    if (ndays < 30 && sum(days) >= 150) {
       forecast_exists =
         any(counts$forecast[counts$petition_id == p$id])
       update_forecast(p, con, days, reg, forecast_exists)
@@ -188,26 +207,20 @@ for (row in 1:nrow(responses)) {
 }
 
 
-# plot petitions (for fun)
-## petplot = function(pets, n) {
-##   creation_date =
-##     as.Date(as.POSIXct(pets$created[n], origin = "1970-01-01"))
-##   ndays = Sys.Date() - creation_date
-##   days = pets[n, paste0("day", 1:30)]
-##   lowers = pets[n, paste0("lower", (ndays + 1):30)]
-##   uppers = pets[n, paste0("upper", (ndays + 1):30)]
-##   subt = paste0("Started ",
-##       as.character(as.Date(as.POSIXct(pets$created[n],
-##                                       origin = "1970-01-01"))),
-##       ", Currently ", pets$signatureCount[n], " Signatures")
-##   plot(1:30, days, type = "l",
-##        ylim = c(0, max(unlist(c(uppers, days)))),
-##        main = pets$title[n], sub = subt,
+## # plot petitions (just checking)
+## petplot = function(pet, counts) {
+##   pet_counts = subset(counts, petition_id == pet$id)
+##   pet_counts = pet_counts[order(pet_counts$date), ]
+##   subt = paste0("Started ", as.character(creation_date),
+##       ", Currently ", pet$signatureCount, " Signatures")
+##   plot(1:30, pet_counts$cumulative, type = "l",
+##        ylim = c(0, 100000),
+##        main = pet$title, sub = subt,
 ##        xlab = "Day", ylab = "Total Signatures")
-##   points(ndays:30, c(days[ndays], lowers), type = "l", col = "blue")
-##   points(ndays:30, c(days[ndays], uppers), type = "l", col = "blue")
+##   points(1:30, pet_counts$upperBound, type = "l", col = "blue")
+##   points(1:30, pet_counts$lowerBound, type = "l", col = "blue")
 ## }
-## n = sample(1:nrow(pets), 1)
-## petplot(pets, 1)
+## pet = petitions[sample(1:nrow(petitions), 1), ]
+## petplot(pet, counts)
 
 dbDisconnect(con)
